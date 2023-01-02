a21624381a95af95a1aa6a7561bf65064e785e1c
NguyenALuy <s3891919@rmit.edu.vn>
2023-01-02 16:35:38 +0700

    add reference

--- models/artists.js
+++ models/artists.js
@@ -1,3 +1,10 @@
+/*************************************************************** 
+*Title: artists
+*Author: Luy Nguyen, Thang Do
+*Date: 20 Dec 2022
+*Code version: V1 
+*Availability: https://github.com/doquangthang-zet/Front-end-Streaming-Web/tree/main/muzikland 
+****************************************************************/ 
 const mongoose = require("mongoose");
 
 const artistSchema = mongoose.Schema({
